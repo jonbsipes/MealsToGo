df52c4c6a9eaeb2ccc824243a03cc650d300bf7b
jonbsipes <jonbsipes@gmail.com>
2024-03-24 19:57:04 -0500

    eslint and prettier changes

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchbar}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.listbox}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  searchbar: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  listbox: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
   },
 });
